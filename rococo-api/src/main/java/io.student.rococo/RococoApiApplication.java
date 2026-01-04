a3ffac78dcca1bf81bbdc7437620f808026ccc4d
olganow <olganow@yandex.ru>
2026-01-04 11:53:26 +0300

    set up rococo api

---
+++ rococo-api/src/main/java/io.student.rococo/RococoApiApplication.java
@@ -0,0 +1,19 @@
+package io.student.rococo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+// Аннотация @SpringBootApplication - комбинированная аннотация Spring Boot
+// Включает в себя:
+// 1. @Configuration - класс содержит конфигурацию Spring
+// 2. @EnableAutoConfiguration - включает автоконфигурацию Spring Boot
+// 3. @ComponentScan - включает сканирование компонентов в текущем пакете и подпакетах
+@SpringBootApplication
+public class RococoApiApplication {
+  public static void main(String[] args) {
+      // Запуск Spring Boot приложения
+      // SpringApplication.run() инициализирует Spring контекст, настраивает сервер и т.д.
+      // PhotocatalogApplication.class - класс с аннотацией @SpringBootApplication
+      // args - аргументы командной строки
+    SpringApplication.run(RococoApiApplication.class);
+  }
+}
