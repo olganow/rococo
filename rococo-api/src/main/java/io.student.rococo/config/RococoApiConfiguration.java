7bd22be8005015bef6feaca61076bdb50958c6be
olganow <olganow@yandex.ru>
2026-01-04 15:09:14 +0300

    api config

---
+++ rococo-api/src/main/java/io.student.rococo/config/RococoApiConfiguration.java
@@ -0,0 +1,27 @@
+package io.student.rococo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class RococoApiConfiguration {
+
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+    http.csrf(AbstractHttpConfigurer::disable)
+        .authorizeHttpRequests(customizer ->
+            customizer.requestMatchers("/api/session").permitAll()
+                .anyRequest().authenticated()
+        )
+        .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+    return http.build();
+  }
+}
